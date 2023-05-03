a234f340652ab5ee160a03b722e95f3f4bc9ce5f
DundalekL <xdundal2@mendelu.cz>
2023-05-03 21:33:35 +0200

    Update 1.1
    Tečka přesnější, KLADNÉ hodnoty v Y nyní posouvají tečku nahoru

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ canvas.pack()
 img = ImageTk.PhotoImage(Image.open("polcompass.jpg"))
 canvas.create_image(0, 0, anchor=NW, image=img)
 
-xpos = (img.width()/2 - 15)
-ypos = (img.height()/2)
+xpos = (img.width()/2 - 14)
+ypos = (img.height()/2 + 1)
 
-xhodnoty = 0
-yhodnoty = 0
+xhodnoty = 100
+yhodnoty = 100
 
 dot = Image.open("dot.png")
-dotres = ImageTk.PhotoImage(dot.resize((9, 9)))
-canvas.create_image(xpos + xhodnoty, ypos + yhodnoty, image=dotres)
+dotres = ImageTk.PhotoImage(dot.resize((8, 8)))
+canvas.create_image(xpos + xhodnoty, ypos - yhodnoty, image=dotres)
 
 
 mainloop()
