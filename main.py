519cad74bdc3906ca8894e9a2736931cdd05c34f
DundalekL <xdundal2@mendelu.cz>
2023-05-03 21:47:05 +0200

    Update 1.2
    Předěláno do 1 blok systému, přihozeny komentáře

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ canvas.create_image(0, 0, anchor=NW, image=img)
 xpos = (img.width()/2 - 14)
 ypos = (img.height()/2 + 1)
 
-xhodnoty = 100
-yhodnoty = 100
+#36x36 velikost bloku kompasu
+xhodnoty = 0
+yhodnoty = 0
 
 dot = Image.open("dot.png")
-dotres = ImageTk.PhotoImage(dot.resize((8, 8)))
-canvas.create_image(xpos + xhodnoty, ypos - yhodnoty, image=dotres)
+dotres = ImageTk.PhotoImage(dot.resize((12, 12))) #12 je ještě akceptovatelné v poměru velikost-přesnost
+canvas.create_image(xpos + (xhodnoty*36), ypos - (yhodnoty*36), image=dotres) #začátek střed, 1 v hodnotách je jeden blok
 
 
 mainloop()
